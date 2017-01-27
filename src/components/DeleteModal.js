d852e426737a4d7c4d5eec6fb63b50f60e0f2c99
Nic Mitchell <nic@nicmitchell.com>
2017-01-26 19:52:21 -0800

    added escape key handler for modal

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 class DeleteModal extends Component {
+  handleKeyPress = (e) => {
+    if (e.key === 'Escape') {
+      this.props.onHide();
+    }
+  }
+
   render() {
     return (
-      <Modal show={ this.props.show } bsSize="small">
-        <Modal.Header><h4>Are you sure?</h4></Modal.Header>
+      <Modal show={ this.props.show } bsSize="small" onKeyUp={ (e) => this.handleKeyPress(e) }>
+        <Modal.Header closeButton ><h4>Are you sure?</h4></Modal.Header>
         <Modal.Body>You are about to delete the credentials for { this.props.account }. Are you sure you want to do this?</Modal.Body>
         <Modal.Footer>
           <Button bsStyle="default" onClick={ this.props.onHide }>Cancel</Button>
